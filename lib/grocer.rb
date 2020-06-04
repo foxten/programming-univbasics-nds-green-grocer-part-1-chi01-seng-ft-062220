dac93cefa5ab4c24859a83914703665035467651
Tenaysia Fox <tenaysiafox@gmail.com>
2020-06-04 23:47:59 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -18,6 +18,8 @@ def consolidate_cart(cart)
     cart[i][:count] = 0
       quantities_item = find_item_by_name_in_collection(cart[i][:item], quantities)
         if 
+        else
+        end
      i +=1
   end
   quantities
