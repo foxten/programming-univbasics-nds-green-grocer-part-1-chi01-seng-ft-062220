54442444b9e9b76b2b021c7a2fef487cecb79282
Tenaysia Fox <tenaysiafox@gmail.com>
2020-06-04 23:04:04 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -17,10 +17,7 @@ consolidated = {}
 cart.each do |list|
   list.each do |item, characteristics|
     consolidated[item]==characteristics
-    if consolidated[item][:count]
-      consolidated[item][:count] +=1
-    else 
-      consolidated[item][:count] = 1
+  consolidated[item][:count] ? consolidated[item][:count] +=1 : consolidated[item][:count] = 1
    end
  end
  quantities << consolidated
